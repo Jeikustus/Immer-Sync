8ec8f55126782620b88242bd3cf8d6730fa115e4
Jeikustus <august.jakelourencevillar@gmail.com>
2024-04-14 22:36:02 +0800

    updated notif for job

--- src/app/(main)/notification/page.tsx
+++ src/app/(main)/notification/page.tsx
@@ -149,7 +149,9 @@ const NotificationPage: React.FC = () => {
               </div>
             </div>
             <button
-              onClick={() => deleteNotification(notification.id)}
+              onClick={() => {
+                window.location.href = "/find-job";
+              }}
               type="button"
               className="ms-auto -mx-1.5 -my-1.5 bg-white text-blue-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8
               w-8"
